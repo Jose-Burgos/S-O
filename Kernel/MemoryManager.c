c54aacad0d7f0ee86ca7ba7f79b710f9185b81c8
CakeH4X <sbraun@itba.edu.ar>
2023-10-19 19:06:05 -0300

    Initial memory manager code

--- Kernel/MemoryManager.c
+++ Kernel/MemoryManager.c
@@ -1,6 +1,6 @@
 #include <MemoryManager.h>
 
-typedef struct MemoryManagerCDT {
+/* typedef struct MemoryManagerCDT {
 	char *nextAddress;
 } MemoryManagerCDT;
 
@@ -21,4 +21,48 @@ void *allocMemory(const uint64_t memoryToAllocate) {
 	memory->nextAddress += memoryToAllocate;
 
 	return (void *) allocation;
+} */
+
+typedef struct MemBlock {
+	size_t size;
+	struct MemBlock *next;
+} MemBlock;
+
+MemBlock *free_mem = NULL;
+MemBlock *alloc_mem = NULL;
+
+int initalizeMemoryManager(void *initialAddress, size_t size) {
+	// Checks for an aligned address, size and enough memory to allocate.
+	if(size < sizeof(MemBlock) + sizeof(size_t) || size % sizeof(size_t) != 0 || (size_t)initialAddress % sizeof(size_t) != 0)
+		return 1;
+
+	//Creates first block and assign it as the first element of the free memory list.
+	MemBlock * first_block = initialAddress;
+	first_block -> size = size - sizeof(MemBlock);
+	first_block -> next = NULL;
+
+	free_mem = first_block;
+
+	infoMem.allocated = sizeof(MemBlock);
+	infoMem.free = first_block -> size;
+	infoMem.total = size;
+
+	return 0;
+}
+
+void *malloc(size_t size) {
+	if(size == 0)
+		return NULL;
+
+	//First fit
+	MemBlock *block = free_mem;
+	while(block != NULL && block -> size < size) {
+		block = block -> next;
+	}
+
+	//Llegamos al final de la lista de free_mem
+	if(block == NULL)
+		return NULL;
+
+	
 }
--- Kernel/include/MemoryManager.h
+++ Kernel/include/MemoryManager.h
@@ -4,10 +4,18 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-typedef struct MemoryManagerCDT *MemoryManagerADT;
+/* typedef struct MemoryManagerCDT *MemoryManagerADT;
 
 MemoryManagerADT createMemoryManager(void *const restrict memoryForMemoryManager, void *const restrict managedMemory);
 
-void *allocMemory(const uint64_t memoryToAllocate);
+void *allocMemory(const uint64_t memoryToAllocate); */
+
+typedef struct info_Mem {
+    size_t allocated;
+    size_t free;
+    size_t total;
+} info_Mem;
+
+static info_Mem infoMem;
 
 #endif
